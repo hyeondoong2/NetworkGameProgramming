ba45d770aaaa9faf29c9ff444c602c23249bf755
hyeondoong2 <sallybi@naver.com>
2025-09-22 20:30:44 +0900

    fix : 함수를 정의해 구현하는것으로 수정

--- homework2/ByteOrder.cpp
+++ homework2/ByteOrder.cpp
@@ -1,11 +1,17 @@
 #include "Common.h"
 #include <iostream>
 
+bool is_big_endian() {
+    uint32_t x = 0x01020304;
+    return (htonl(x) == x);
+}
+
+
 int main(int argc, char* argv[])
 {
     uint32_t x = 0x01020304;
 
-    if (htonl(x) == x) {
+    if (is_big_endian()) {
         std::cout << "Big-endian" << '\n';
     }
     else {
